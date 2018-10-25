3665e8f809588ccca3f33340c4682e93db63fbff
abdulrasaq99 <sheunvi@gmail.com>
2018-10-25 02:10:48 -0700

    Thursday 25/10/18 10:10 PM

---
+++ jslib/script-one.js
@@ -0,0 +1,18 @@
+//Script for page
+
+$(document).ready(
+    function(){
+    //text effect
+    var effectdiv
+
+    var effectdiv = new Typed('#textfx .shower', {
+        strings: ['Technology for <strong class="xfx">Business</strong>', 'Tools for <strong class="xfx">Engineering</strong>', 'Software and Embedded solutions'],
+        typeSpeed: 0,
+        backSpeed: 0,
+        smartBackspace: true, // this is a default
+        loop: true
+      });
+        
+
+    }
+);
