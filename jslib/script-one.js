507181035f6336bc78bf0fe53a2d9552602735b2
numi1 <numinetech@hotmail.com>
2021-06-06 16:25:28 +0100

    javascript Error Removal

--- jslib/script-one.js
+++ jslib/script-one.js
@@ -1,23 +1,5 @@
 //Script for page
 
 $(document).ready(
-    function(){
-    //text effect
-    var effectdiv
 
-    var effectdiv = new Typed('#textfx .shower', {
-        strings: ['Technology for <strong class="xfx">Business</strong>', 'Tools for <strong class="xfx">Development</strong>', 'Creativity in <strong class="xfx">Design</strong>'],
-        typeSpeed: 0,
-        backSpeed: 0,
-        smartBackspace: true, // this is a default
-        loop: true
-      });
-      
-    //link active
-    $('.menu .nav-item').click(function(e){
-        $('.menu .nav-item').css("color", "black");
-        $(this).css("color", "#650101");
-    });
-
-    }
 );
