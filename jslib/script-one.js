0796ef8c951505b35620f475236d0e5cc341a968
numi1 <numinetech@hotmail.com>
2021-12-28 12:02:22 +0100

    Software & Design

--- jslib/script-one.js
+++ jslib/script-one.js
@@ -1,5 +1,9 @@
 //Script for page
 
 $(document).ready(
-
+    function(){
+        document.getElementById('mail-link').onclick = function(){
+            window.open('mailto:numinetech@hotmail.com', '_blank');
+          };
+    }
 );
